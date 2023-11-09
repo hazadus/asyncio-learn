43d34163d3c4d0fe7d84c286b960342340d97c41
hazadus <hazadus7@gmail.com>
2023-11-09 19:26:33 +0300

    Added listing 2.13.

---
+++ listing_2_13.py
@@ -0,0 +1,20 @@
+"""
+Защита задачи от снятия
+"""
+import asyncio
+from util import delay
+
+
+async def main() -> None:
+    delay_task = asyncio.create_task(delay(10))
+
+    try:
+        result = await asyncio.wait_for(asyncio.shield(delay_task), timeout=5)
+        print(result)
+    except asyncio.exceptions.TimeoutError:
+        print("Task taken more than 5 sec, it will end soon!")
+        result = await delay_task
+        print(result)
+
+
+asyncio.run(main())
