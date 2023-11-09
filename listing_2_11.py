8968703364208107c658c97e393d8d59b2786a07
hazadus <hazadus7@gmail.com>
2023-11-09 19:12:18 +0300

    Added listing 2.11.

---
+++ listing_2_11.py
@@ -0,0 +1,28 @@
+"""
+Снятие задачи
+"""
+import asyncio
+from asyncio import CancelledError
+from util import delay
+
+
+async def main():
+    long_task = asyncio.create_task(delay(10))
+
+    seconds_elapsed = 0
+
+    while not long_task.done():
+        print("Task is not done, next check after one second...")
+        await asyncio.sleep(1)
+        seconds_elapsed += 1
+
+        if seconds_elapsed == 5:
+            long_task.cancel()
+
+    try:
+        await long_task
+    except CancelledError:
+        print("Task was cancelled.")
+
+
+asyncio.run(main())
