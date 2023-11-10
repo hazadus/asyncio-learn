b9e57b7a86c0f10c925688b81418dfa5281a371e
hazadus <hazadus7@gmail.com>
2023-11-10 09:26:15 +0300

    Added listing 2.19.

---
+++ listing_2_19.py
@@ -0,0 +1,37 @@
+"""
+Счетный код и длительная задача
+
+Sample output:
+Calling <function main at 0x104cc76d0> with arguments () {}
+Calling <function cpu_bound_work at 0x104732f80> with arguments () {}
+<function cpu_bound_work at 0x104732f80> completed in 3.8624 sec.
+Calling <function cpu_bound_work at 0x104732f80> with arguments () {}
+<function cpu_bound_work at 0x104732f80> completed in 3.8587 sec.
+Starting sleep for 4 sec...
+End sleeping for 4 sec.
+<function main at 0x104cc76d0> completed in 11.7227 sec.
+"""
+import asyncio
+from util import async_timed, delay
+
+
+@async_timed()
+async def cpu_bound_work() -> int:
+    counter = 0
+    for i in range(10**8):
+        counter += 1
+    return counter
+
+
+@async_timed()
+async def main() -> None:
+    task1 = asyncio.create_task(cpu_bound_work())
+    task2 = asyncio.create_task(cpu_bound_work())
+    delay_task = asyncio.create_task(delay(4))
+
+    await task1
+    await task2
+    await delay_task
+
+
+asyncio.run(main())
