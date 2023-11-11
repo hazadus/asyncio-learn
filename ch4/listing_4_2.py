f1388632c2d133f1f0e6ed5b2833ebc9a414f1b1
hazadus <hazadus7@gmail.com>
2023-11-11 21:20:16 +0300

    Added actual listing 4.2.

---
+++ ch4/listing_4_2.py
@@ -0,0 +1,23 @@
+"""
+Отправка веб-запроса с по мощью aiohttp
+"""
+import asyncio
+from aiohttp import ClientSession
+from util import async_timed
+
+
+@async_timed()
+async def fetch_status(session: ClientSession, url: str) -> int:
+    async with session.get(url) as result:
+        return result.status
+
+
+@async_timed()
+async def main() -> None:
+    async with ClientSession() as session:
+        url = "https://hazadus.ru/repos/"
+        status = await fetch_status(session, url)
+        print(f"Status of {url} is '{status}'")
+
+
+asyncio.run(main())
