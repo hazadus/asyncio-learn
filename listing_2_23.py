aaae5b012ec795ed0ce2693703795d39e0989736
hazadus <hazadus7@gmail.com>
2023-11-11 11:22:05 +0300

    Added listing 2.23.

---
+++ listing_2_23.py
@@ -0,0 +1,23 @@
+"""
+Выполнение счетного кода в отладочном режиме
+"""
+import asyncio
+from util import async_timed
+
+
+@async_timed()
+async def cpu_bound_work() -> int:
+    counter = 0
+    for i in range(10**8):
+        counter += 1
+    return counter
+
+
+async def main() -> None:
+    loop = asyncio.get_event_loop()
+    loop.slow_callback_duration = 0.250
+    task1 = asyncio.create_task(cpu_bound_work())
+    await task1
+
+
+asyncio.run(main(), debug=True)
