a9a2257d524c7afd233497b3d00611ee347618cd
hazadus <hazadus7@gmail.com>
2023-11-11 21:38:42 +0300

    Added listing 4.5.

---
+++ ch4/listing_4_5.py
@@ -0,0 +1,15 @@
+"""
+Использование спискового включения для конкурентного выполнения задач
+"""
+import asyncio
+from util import async_timed, delay
+
+
+@async_timed()
+async def main() -> None:
+    delay_times = [3, 2, 1]
+    tasks = [asyncio.create_task(delay(seconds)) for seconds in delay_times]
+    [await task for task in tasks]
+
+
+asyncio.run(main())
