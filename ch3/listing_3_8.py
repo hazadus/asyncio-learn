4b560291c1d9e149c005046e8716ed95ad88e2b7
hazadus <hazadus7@gmail.com>
2023-11-11 17:27:57 +0300

    Added listing 3.8.

---
+++ ch3/listing_3_8.py
@@ -0,0 +1,43 @@
+"""
+Построение асинхронного эхо-сервера
+"""
+import asyncio
+import socket
+from asyncio import AbstractEventLoop
+
+BUFFER_SIZE = 1024
+
+
+async def echo(connection: socket, loop: AbstractEventLoop) -> None:
+    """
+    Wait for data from the client and send it back.
+    """
+    while data := await loop.sock_recv(connection, BUFFER_SIZE):
+        await loop.sock_sendall(connection, data)
+
+
+async def listen_for_connection(server_socket: socket, loop: AbstractEventLoop) -> None:
+    """
+    Listen for connections, and create `echo` task for each connection.
+    """
+    while True:
+        connection, address = await loop.sock_accept(server_socket)
+        connection.setblocking(False)
+        print(f"Connection from {address}")
+        asyncio.create_task(echo(connection, loop))
+
+
+async def main() -> None:
+    # Create TCP server:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = ("127.0.0.1", 8000)
+    server_socket.setblocking(False)  # Mark server socket as non-blocking
+    server_socket.bind(server_address)
+    server_socket.listen()
+
+    await listen_for_connection(server_socket, asyncio.get_event_loop())
+
+
+asyncio.run(main())
