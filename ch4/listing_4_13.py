31a5f7125beddcfa62f85889f7ee49efc8a39795
hazadus <hazadus7@gmail.com>
2023-11-12 12:57:46 +0300

    Added listing 4.13.

---
+++ ch4/listing_4_13.py
@@ -0,0 +1,38 @@
+"""
+Обработка запросов по мере завершения
+"""
+import asyncio
+from aiohttp import ClientSession
+from util import async_timed
+
+
+@async_timed()
+async def fetch_status(session: ClientSession, url: str, delay: int = 0) -> int:
+    await asyncio.sleep(delay)
+    async with session.get(url) as result:
+        return result.status
+
+
+@async_timed()
+async def main() -> None:
+    async with ClientSession() as session:
+        fetchers = [
+            asyncio.create_task(fetch_status(session, url="https://hazadus.ru")),
+            asyncio.create_task(fetch_status(session, url="https://hazadus.ru")),
+            asyncio.create_task(fetch_status(session, url="https://hazadus.ru")),
+            asyncio.create_task(fetch_status(session, url="https://hazadus.ru/repos/")),
+        ]
+
+        # Return from `wait` on first completed task:
+        done, pending = await asyncio.wait(
+            fetchers, return_when=asyncio.FIRST_COMPLETED
+        )
+
+        print(f"Done tasks...: {len(done)}")
+        print(f"Pending tasks: {len(pending)}")
+
+        for done_task in done:
+            print(await done_task)
+
+
+asyncio.run(main())
